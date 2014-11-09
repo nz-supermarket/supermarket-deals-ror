036663498e311d4868650b5cb7f1c2c7f0739690
Daniel Leong <daniel.ltw@gmail.com>
2014-11-09 21:58:33 +1300

    - including other javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require vendor/angular.js
+//= require vendor/ng-table.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
