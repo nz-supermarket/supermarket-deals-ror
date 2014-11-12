4ae0274593ea4a34463be1bae6fd62cf69576550
Daniel Leong <daniel.ltw@gmail.com>
2014-11-12 21:34:58 +1300

    test removal

--- config/initializers/iron_worker.rb
+++
@@ -1,7 +0,0 @@
-IronWorker.configure do |config|
-  config.token = ENV['IRON_WORKER_TOKEN']
-  config.project_id = ENV['IRON_WORKER_PROJECT_ID']
-
-  # Use the line below if you're using an ActiveRecord database
-  config.database = Rails.configuration.database_configuration[Rails.env]
-end
