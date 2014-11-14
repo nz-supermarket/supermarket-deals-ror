0101dd4677b72801af900f2ecf5d0d76009bac15
Daniel Leong <daniel.ltw@gmail.com>
2014-11-14 21:33:51 +1300

    production config update for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
