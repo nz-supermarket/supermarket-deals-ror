c05edaceb3be13365e17431d20e1442bca028b95
Daniel Leong <daniel.ltw@gmail.com>
2014-11-30 08:09:01 +1300

    - remove helpers and gon

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,10 @@
 class ProductsController < ApplicationController
-  helper_method :sort_column, :sort_direction
   before_action :set_product, only: [:show]
 
   # GET /products
   # GET /products.json
   def index
     @counter = ((Time.now - Time.at(1414321201)).to_i / 604800).round
-    @products = Product.all
-    gon.products = @products
   end
 
   # GET /products/1
