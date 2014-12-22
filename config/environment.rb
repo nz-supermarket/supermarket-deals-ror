4480f8103403d843838595a1e59d6b57daf2afa2
Daniel Leong <danielleong@Daniels-MacBook-Air.local>
2014-12-22 19:24:39 +1300

    wrap config into Rails config

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Remote Logging on Paper Trail
-config.logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 53786, :program => "rails-#{RAILS_ENV}")
+Rails.application.configure do
+  # Remote Logging on Paper Trail
+  config.logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 53786, :program => "rails-#{RAILS_ENV}")
+end
