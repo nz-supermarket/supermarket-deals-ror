bcf0625640ecab2c7c0fabc8e0848698fcdba78c
Daniel Leong <daniel.ltw@gmail.com>
2015-01-24 14:45:32 +1300

    - pull products.link_to_cd as well

--- db/migrate/20150123224306_new_prices_view.rb
+++ db/migrate/20150123224306_new_prices_view.rb
@@ -1,7 +1,15 @@
 class NewPricesView < ActiveRecord::Migration
   def up
     execute <<-SQL
-    CREATE VIEW lowest_prices AS SELECT (SELECT prices FROM normal_prics) AS normal, (SELECT prices FROM special_prices) AS special 
+    CREATE VIEW lowest_prices AS SELECT products.id,
+      products.name, products.volume, products.sku, products.aisle, products.link_to_cd
+      (SELECT price FROM normal_prices where product_id = products.id ORDER BY normal_prices.price LIMIT 1) AS normal_price,
+      (SELECT date FROM normal_prices where product_id = products.id ORDER BY normal_prices.price LIMIT 1) AS normal_date,
+      (SELECT price FROM special_prices where product_id = products.id ORDER BY special_prices.price LIMIT 1) AS special_price,
+      (SELECT date FROM special_prices where product_id = products.id ORDER BY special_prices.price LIMIT 1) AS special_date,
+      ((SELECT price FROM normal_prices where product_id = products.id ORDER BY normal_prices.price LIMIT 1) - (SELECT price FROM special_prices where product_id = products.id ORDER BY special_prices.price LIMIT 1)) AS diff_price,
+      (((SELECT price FROM normal_prices where product_id = products.id ORDER BY normal_prices.price LIMIT 1) - (SELECT price FROM special_prices where product_id = products.id ORDER BY special_prices.price LIMIT 1)) / (SELECT price FROM normal_prices where product_id = products.id ORDER BY normal_prices.price LIMIT 1)) AS discount
+      FROM products;
     SQL
   end
   def down
