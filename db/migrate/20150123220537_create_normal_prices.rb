2f95ea715ca838dd5231019ed2d4df46771a8ae5
Daniel Leong <daniel.ltw@gmail.com>
2015-01-24 14:40:43 +1300

    - date rather than timestamp

--- db/migrate/20150123220537_create_normal_prices.rb
+++ db/migrate/20150123220537_create_normal_prices.rb
@@ -2,7 +2,7 @@ class CreateNormalPrices < ActiveRecord::Migration
   def change
     create_table :normal_prices do |t|
       t.decimal :price, precision: 8, scale: 3
-      t.timestamp :date
+      t.date :date
       t.references :product, index: true
     end
   end
--- db/migrate/20150123220634_create_special_prices.rb
+++ db/migrate/20150123220634_create_special_prices.rb
@@ -2,7 +2,7 @@ class CreateSpecialPrices < ActiveRecord::Migration
   def change
     create_table :special_prices do |t|
       t.decimal :price, precision: 8, scale: 3
-      t.timestamp :date
+      t.date :date
       t.references :product, index: true
     end
   end
