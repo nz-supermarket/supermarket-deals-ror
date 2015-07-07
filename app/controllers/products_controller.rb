6191e00fdd6d54f32991bed0cbff0a842dc5ceeb
Daniel Leong <Daniel.ltw@gmail.com>
2015-07-08 08:35:11 +1200

    - primary key issue, use where search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,7 @@ class ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = LowestPrice.find(params[:id])
+      @product = LowestPrice.where(id: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
