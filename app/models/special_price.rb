3b3a734d10ced63460dbf0181a5edf8d9c82f6f6
Daniel Leong <Daniel.ltw@gmail.com>
2015-07-09 15:16:44 +1200

    - remove common stuff and extend price.rb

--- app/models/special_price.rb
+++ app/models/special_price.rb
@@ -1,14 +1,5 @@
 # Special Prices Records
-class SpecialPrice < ActiveRecord::Base
-  belongs_to :product
-  before_validation :date_fix
-
+class SpecialPrice < Price
   alias_attribute :special, :price
   alias_attribute :special_date, :date
-
-  validates_uniqueness_of :product_id, :scope => :date
-
-  def date_fix
-    self.date = Time.zone.now.to_date
-  end
 end
