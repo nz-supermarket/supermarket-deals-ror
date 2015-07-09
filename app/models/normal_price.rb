fc5dd8b6e56d88a162df20ea65fd9ca197eb836a
Daniel Leong <Daniel.ltw@gmail.com>
2015-07-09 15:16:19 +1200

    - remove common stuff and extend price.rb

--- app/models/normal_price.rb
+++ app/models/normal_price.rb
@@ -1,14 +1,5 @@
 # Normal Prices Records
-class NormalPrice < ActiveRecord::Base
-  belongs_to :product
-  before_validation :date_fix
-
+class NormalPrice < Price
   alias_attribute :normal, :price
   alias_attribute :normal_date, :date
-
-  validates_uniqueness_of :product_id, :scope => :date
-
-  def date_fix
-    self.date = Time.zone.now.to_date
-  end
 end
