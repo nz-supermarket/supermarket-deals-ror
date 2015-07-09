cf57e8e294f98413ea6162d6991f7d7fb0b9e545
Daniel Leong <Daniel.ltw@gmail.com>
2015-07-09 15:38:39 +1200

    - fix zone

--- lib/modules/midnight.rb
+++ lib/modules/midnight.rb
@@ -1,5 +1,5 @@
 module Midnight
   def seconds_to_midnight
-    Time.new(Time.now.year, Time.now.month, Time.now.day, 23, 58, 00) - Time.now
+    Time.new(Time.zone.now.year, Time.zone.now.month, Time.zone.now.day, 23, 58, 00) - Time.zone.now
   end
 end
