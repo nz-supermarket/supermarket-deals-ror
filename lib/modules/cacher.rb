4160005840721c969dcbe1c0247c373dec60b1d0
Daniel Leong <daniel.ltw@gmail.com>
2015-07-11 09:01:49 +1200

    - ensure condition does what it should

--- lib/modules/cacher.rb
+++ lib/modules/cacher.rb
@@ -10,7 +10,7 @@ module Cacher
   def cache_retrieve_url(cache, val)
 
     if cache.fetch(val).present?
-      return cache.fetch(val) if cache.fetch(val).match(/(\s500\s)/) # match " 500 " for 500 error
+      return cache.fetch(val) if cache.fetch(val).match(/(\s500\s)/).nil? # match " 500 " for 500 error
     end
 
     cache.delete(val)
