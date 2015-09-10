8818d70d0d985efc6cb3cf03f0197af1b12e46b8
Daniel Leong <daniel.ltw@gmail.com>
2015-09-10 19:06:41 +1200

    - break up price columns for sorting

--- app/datatables/products_datatable.rb
+++ app/datatables/products_datatable.rb
@@ -57,7 +57,7 @@ class ProductsDatatable
 
   def sort_column
     # database views/tables column names
-    columns = %w[name volume sku diff_price aisle discount]
+    columns = %w[name volume sku special normal diff_price aisle discount]
     columns[params[:iSortCol_0].to_i]
   end
 
