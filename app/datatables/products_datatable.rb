9e87b78ccbd6e4064093858bcfd029b2d87dc503
Daniel Leong <daniel.ltw@gmail.com>
2015-09-11 18:55:12 +1200

    - add price handler column back in

--- app/datatables/products_datatable.rb
+++ app/datatables/products_datatable.rb
@@ -21,6 +21,7 @@ class ProductsDatatable
         link_to(product.name, "/products/#{product.id}"),
         product.volume,
         product.sku,
+        price_handler(product.special, product.normal, product.diff),
         number_to_currency(product.special),
         number_to_currency(product.normal),
         number_to_currency(product.diff),
