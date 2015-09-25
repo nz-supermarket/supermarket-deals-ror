60c1e8dff60ca7ddb07337b2fc89e8cabba57005
Daniel Leong <daniel.ltw@gmail.com>
2015-09-26 08:59:26 +1200

    - add test to test the ability to pick up links from homapage

--- spec/rake_price_fetch_spec.rb
+++ spec/rake_price_fetch_spec.rb
@@ -21,3 +21,23 @@ describe 'rproxy, webscrape, countdown homepage' do
     end
   end
 end
+
+describe 'rproxy, webscrape, countdown links' do
+  before do
+    require "#{Rails.root}/lib/modules/countdown_links_processor"
+    require "#{Rails.root}/lib/modules/countdown_aisle_processor"
+  end
+
+  it 'should be able to process various links' do
+    cache = ActiveSupport::Cache::FileStore.new('/tmp')
+    VCR.use_cassette('fetch_homepage', :match_requests_on => [:method, :uri, :query]) do
+      doc = CountdownAisleProcessor.home_doc_fetch
+      VCR.use_cassette('aisles', :match_requests_on => [:method, :uri, :query]) do
+        aisles = CountdownLinksProcessor.generate_aisle(doc, cache)
+        expect(aisles.size).to eq(3120)
+        expect(aisles.first).to include('/Shop/Browse/bakery/')
+        expect(aisles.last).to include('/Shop/Browse/toys-party-needs/')
+      end
+    end
+  end
+end
