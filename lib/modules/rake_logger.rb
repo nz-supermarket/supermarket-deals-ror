8f1c9841e19be1ef49a1c4de6304e46db686b012
Daniel Leong <daniel.ltw@gmail.com>
2015-09-26 11:10:04 +1200

    - refactor of code, based on rubocop

--- lib/modules/rake_logger.rb
+++ lib/modules/rake_logger.rb
@@ -1,12 +1,11 @@
+# processing log string based on log level
 module RakeLogger
   def logger(string, level = 'debug')
-    if level == "debug"
+    if level == 'debug'
       log_string string
-    elsif level == "info"
-      unless string.include? "Unable"
-        log_string string
-      end
-    elsif level == "simple"
+    elsif level == 'info'
+      log_string string unless string.include? 'Unable'
+    elsif level == 'simple'
       print('.')
     end
   end
@@ -14,17 +13,8 @@ module RakeLogger
   private
 
   def log_string(string)
-    if string.include? "exist"
-      unless @log_string_builder.include? "exist"
-        @log_string_builder = string
-      else
-        @log_string_builder = @log_string_builder.gsub('. ', '')
-        @log_string_builder = @log_string_builder + string.gsub("Product exist with sku: ", ", ")
-      end
-    else
-      @log_string_builder = @log_string_builder + '\n'
-      @log_string_builder = @log_string_builder + string
-      info string
-    end
+    @log_string_builder += '\n'
+    @log_string_builder += string
+    info string
   end
 end
