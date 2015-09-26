978ed95357aa53469ec845268c1c2147912acc87
Daniel Leong <daniel.ltw@gmail.com>
2015-09-26 15:24:37 +1200

    - spec test for specific cell types when scraping

--- spec/rake_price_fetch_spec.rb
+++ spec/rake_price_fetch_spec.rb
@@ -41,3 +41,47 @@ describe 'rproxy, webscrape, countdown links' do
     end
   end
 end
+
+describe 'rproxy, webscrape, countdown aisles' do
+  before :each do
+    require "#{Rails.root}/lib/modules/countdown_aisle_processor"
+    require "#{Rails.root}/lib/modules/rake_logger"
+    @cache = ActiveSupport::Cache::FileStore.new('/tmp')
+    @cap = CountdownAisleProcessor.new
+    @logger = RakeLogger.new
+  end
+
+  it 'should be able to process special price product' do
+    doc = Cacher.cache_retrieve_url(@cache, '/Shop/Deals/bakery/bread-rolls-bread-sticks-bagels/bagels')
+    html = Nokogiri::HTML(doc)
+    aisle = @cap.aisle_name(html)
+    items = html.css('div.product-stamp.product-stamp-grid')
+
+    expect(aisle).to eq('deals, bakery, bread rolls, bread sticks & bagels, bagels')
+    expect(items.count).to eq(1)
+    expect(@cap.special_price?(items.first)).to eq(true)
+    expect(@cap.process_item(items.first, aisle, @logger)).to eq(true)
+
+    expect(NormalPrice.all.size).to eq(1)
+    expect(NormalPrice.where(product_id: 1).first.price).to eq(5.99)
+    expect(SpecialPrice.all.size).to eq(1)
+    expect(SpecialPrice.where(product_id: 1).first.price).to eq(5.50)
+  end
+
+  it 'should be able to process basic multi buy product' do
+    doc = Cacher.cache_retrieve_url(@cache, '/Shop/Deals/bakery/bread-rolls-bread-sticks-bagels/baguette')
+    html = Nokogiri::HTML(doc)
+    aisle = @cap.aisle_name(html)
+    items = html.css('div.product-stamp.product-stamp-grid')
+
+    expect(aisle).to eq('deals, bakery, bread rolls, bread sticks & bagels, baguette')
+    expect(items.count).to eq(1)
+    expect(@cap.special_price?(items.first)).to eq(false)
+    expect(@cap.process_item(items.first, aisle, @logger)).to eq(true)
+
+    expect(NormalPrice.all.size).to eq(2)
+    expect(NormalPrice.where(product_id: 2).first.price).to eq(1.99)
+    expect(SpecialPrice.all.size).to eq(2)
+    expect(SpecialPrice.where(product_id: 2).first.price).to eq(1.50)
+  end
+end
