cd0d8baeec17518a70963f9c43e400f1928cbe0c
Daniel Leong <daniel.ltw@gmail.com>
2015-09-26 15:25:23 +1200

    - use specific rails logger when in test env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if Rails.env = 'test'
+  Rails.logger = Logger.new(STDOUT)
+  Rails.logger.level = 1
+end
