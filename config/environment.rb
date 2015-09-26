3092f8f876bcb87c713c435578acbcfbf8526dc6
Daniel Leong <daniel.ltw@gmail.com>
2015-09-26 16:03:49 +1200

    - fix syntax error

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-if Rails.env = 'test'
+if Rails.env == 'test'
   Rails.logger = Logger.new(STDOUT)
   Rails.logger.level = 1
 end
