3dbd04fe7efcd2b61dc7fa3ac9893e240b60754e
Daniel Leong <daniel.ltw@gmail.com>
2015-10-03 08:54:42 +1300

    - set rails logger to stdout

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-if Rails.env == 'test' || Rails.env == 'development'
-  Rails.logger = Logger.new(STDOUT)
-  Rails.logger.level = 1
-end
+Rails.logger = Logger.new(STDOUT)
+Rails.logger.level = 0
