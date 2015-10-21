049e27bc545af1786af6e5fb064c6ae95345be6e
Daniel Leong <daniel.ltw@gmail.com>
2015-10-21 21:34:57 +1300

    - fix names

--- spec/models/special_price_spec.rb
+++ spec/models/special_price_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SpecialPrices, :type => :model do
+RSpec.describe SpecialPrice, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
