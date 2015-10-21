282fd0e2e1a436e1154a503f66b8d1c370cf08a2
Daniel Leong <daniel.ltw@gmail.com>
2015-10-21 21:33:42 +1300

    - remove redundant variable

--- lib/modules/web_scrape.rb
+++ lib/modules/web_scrape.rb
@@ -4,6 +4,6 @@ require "#{Rails.root}/lib/modules/r_proxy"
 module WebScrape
   include RProxy
   def nokogiri_open_url(url)
-    return Nokogiri::HTML(RProxy.open_url_with_proxy(url, @aisle_processing))
+    return Nokogiri::HTML(RProxy.open_url_with_proxy(url))
   end
 end
