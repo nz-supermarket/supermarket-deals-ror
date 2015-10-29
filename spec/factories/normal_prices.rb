d0dea41792682b4ac55f0d38260eb30d50296bf9
Daniel Leong <daniel.ltw@gmail.com>
2015-10-29 20:17:06 +1300

    - rename class

--- spec/factories/normal_prices.rb
+++ spec/factories/normal_prices.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :normal_price, :class => 'NormalPrices' do
+  factory :normal_price, :class => 'NormalPrice' do
     price "9.99"
     date "2015-01-24 11:05:37"
     product nil
--- spec/factories/special_prices.rb
+++ spec/factories/special_prices.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :special_price, :class => 'SpecialPrices' do
+  factory :special_price, :class => 'SpecialPrice' do
     price "9.99"
     date "2015-01-24 11:06:34"
     product nil
