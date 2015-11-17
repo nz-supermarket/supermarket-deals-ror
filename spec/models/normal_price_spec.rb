ab542c927ee9a24dec049363da929f6e1e6474ed
Daniel Leong <daniel.ltw@gmail.com>
2015-11-17 20:32:17 +1300

    - add general test

--- spec/models/normal_price_spec.rb
+++ spec/models/normal_price_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe NormalPrice, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    create_list(:normal_price, 50)
+  end
+
+  describe 'NormalPrice' do
+    it 'should have 50 prices' do
+      expect(NormalPrice.all.size).to eq(50)
+    end
+
+    it 'should return a result for product_price_history' do
+      expect(NormalPrice.product_price_history(1).size).to eq(1)
+    end
+  end
 end
