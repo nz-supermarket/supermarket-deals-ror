760dbed6293e94181ea6036ae29e7f9bdfa3b8d8
Daniel Leong <daniel.ltw@gmail.com>
2016-02-17 04:54:29 +1300

    - fix sitemap with rw path

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://supermarket.lowestprice.club"
+SitemapGenerator::Sitemap.public_path = 'sitemap/'
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
