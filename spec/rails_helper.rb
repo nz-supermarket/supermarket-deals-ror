342c6a35a1188062587fd0d931d043e0ef9805ec
Daniel Leong <daniel.ltw@gmail.com>
2016-02-27 13:49:11 +1300

    - remove transactional

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,12 +36,12 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  # config.use_transactional_fixtures = false
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
