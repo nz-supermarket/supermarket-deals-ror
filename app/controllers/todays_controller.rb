dc406f91943b61616096d62e64f58db01a25f2bd
Daniel Leong <daniel.ltw@gmail.com>
2016-03-09 20:34:05 +1300

    - exclude last bit of the aisle which could include the product name

--- app/controllers/todays_controller.rb
+++ app/controllers/todays_controller.rb
@@ -6,7 +6,9 @@ class TodaysController < ApplicationController
     result = LowestPrice
              .where(table[:normal_date].eq(Date.today)
                       .or(table[:special_date].eq(Date.today)))
-    result = result.all.group_by(&:aisle)
+    result = result.all.group_by do |item|
+      item.aisle.split(',').first(3).join(',')
+    end
     respond_with result.to_json
   end
 end
