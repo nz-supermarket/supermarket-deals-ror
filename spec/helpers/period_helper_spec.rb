8ab218ccc066718f8c4ee0a655168819e30b9f2d
Daniel Leong <daniel.ltw@gmail.com>
2016-04-02 06:22:36 +1300

    - fix test value to reflect the right outcome

--- spec/helpers/period_helper_spec.rb
+++ spec/helpers/period_helper_spec.rb
@@ -18,16 +18,16 @@ RSpec.describe PeriodHelper, type: :helper do
       expect(human_period(1_296_000)).to eq('2 weeks, 1 day')
     end
 
-    it 'should return 1 month if given 2_628_000' do
-      expect(human_period(2_628_000)).to eq('1 month')
+    it 'should return 1 month if given 2_419_200' do
+      expect(human_period(2_419_200)).to eq('1 month')
     end
 
-    it 'should return 2 months, 2 days if given 5_428_800' do
-      expect(human_period(5_428_800)).to eq('2 months, 2 days')
+    it 'should return 2 months, 2 days if given 5_011_200' do
+      expect(human_period(5_011_200)).to eq('2 months, 2 days')
     end
 
-    it 'should return 1 year, 2 months, 1 week, 2 days if given 37_569_600' do
-      expect(human_period(37_569_600)).to eq('1 year, 2 months, 1 week, 2 days')
+    it 'should return 1 year, 2 months, 1 week, 2 days if given 37_137_600' do
+      expect(human_period(37_137_600)).to eq('1 year, 2 months, 1 week, 2 days')
     end
   end
 end
