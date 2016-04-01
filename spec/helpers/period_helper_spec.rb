4eff43ef977cd882805839e30b5e7a99373c9823
Daniel Leong <daniel.ltw@gmail.com>
2016-04-02 05:22:51 +1300

    - rename method that make more sense

--- spec/helpers/period_helper_spec.rb
+++ spec/helpers/period_helper_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe PeriodHelper, type: :helper do
-  describe '#period_breakdown' do
+  describe '#human_period' do
     it 'should return 1 day if given 86_400' do
-      expect(period_breakdown(86_400)).to eq('1 day')
+      expect(human_period(86_400)).to eq('1 day')
     end
 
     it 'should return 2 days if given 172_800' do
-      expect(period_breakdown(172_800)).to eq('2 days')
+      expect(human_period(172_800)).to eq('2 days')
     end
 
     it 'should return 1 week if given 604_800' do
-      expect(period_breakdown(604_800)).to eq('1 week')
+      expect(human_period(604_800)).to eq('1 week')
     end
 
     it 'should return 2 weeks, 1 day if given 1_296_000' do
-      expect(period_breakdown(1_296_000)).to eq('2 weeks, 1 day')
+      expect(human_period(1_296_000)).to eq('2 weeks, 1 day')
     end
 
     it 'should return 1 month if given 2_628_000' do
-      expect(period_breakdown(2_628_000)).to eq('1 month')
+      expect(human_period(2_628_000)).to eq('1 month')
     end
 
     it 'should return 2 months, 2 days if given 5_428_800' do
-      expect(period_breakdown(5_428_800)).to eq('2 months, 2 days')
+      expect(human_period(5_428_800)).to eq('2 months, 2 days')
     end
 
     it 'should return 1 year, 2 months, 1 week, 2 days if given 37_569_600' do
-      expect(period_breakdown(37_569_600)).to eq('1 year, 2 months, 1 week, 2 days')
+      expect(human_period(37_569_600)).to eq('1 year, 2 months, 1 week, 2 days')
     end
   end
 end
