f314c7be1568d1416c815bd9fea76ee2af9c58c6
Daniel Leong <daniel.ltw@gmail.com>
2016-08-31 06:44:42 +1200

    - use actual sitemap generator objects and classes

--- app/jobs/sitemap_job.rb
+++ app/jobs/sitemap_job.rb
@@ -1,8 +1,11 @@
+require 'sitemap_generator'
 class SitemapJob < ActiveJob::Base
   queue_as :sitemap
 
   def perform(*args)
     Rails.logger.info('***** Sitemap Refresh *****')
-    system 'rake sitemap:refresh'
+
+    SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => verbose)
+    SitemapGenerator::Sitemap.ping_search_engines
   end
 end
