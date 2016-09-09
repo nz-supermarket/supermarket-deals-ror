fcec3d25f2266d2202bc8522a8c45d9b92fff39f
Daniel Leong <daniel.ltw@gmail.com>
2016-09-09 18:07:10 +1200

    - change default worker options

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,6 +6,7 @@ Sidekiq.configure_server do |config|
   config.on(:startup) do
     Sidekiq.schedule = YAML.load_file(File.expand_path("../../schedule.yml",__FILE__))
     Sidekiq::Scheduler.reload_schedule!
+    Sidekiq.default_worker_options = { backtrace: 10, retry: false }
   end
 end
 
