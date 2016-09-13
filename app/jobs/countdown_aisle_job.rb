3b173a61682e17e50f3a95c405e3413f5a927f72
Daniel Leong <Daniel.ltw@gmail.com>
2016-09-13 14:02:41 +1200

    - add another queue to set different priority to various task

--- app/jobs/countdown_aisle_job.rb
+++ app/jobs/countdown_aisle_job.rb
@@ -3,7 +3,7 @@ require 'dalli'
 require "#{Rails.root}/lib/modules/countdown/aisle_processor"
 class CountdownAisleJob
   include Sidekiq::Worker
-  sidekiq_options queue: :countdown
+  sidekiq_options queue: :countdownitem
 
   def perform(*args)
     Rails.logger.info('***** Countdown Aisle *****')
