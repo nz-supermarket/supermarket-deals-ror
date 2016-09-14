7490fb68d83719ad193b818d561dbdc8ebdfb154
Daniel Leong <daniel.ltw@gmail.com>
2016-09-14 18:51:54 +1200

    - change sidekiq worker default options

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,13 @@
 require 'sidekiq/scheduler'
 
+Sidekiq.default_worker_options = { backtrace: 10, retry: 10, failures: :exhausted }
+
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 70) { Redis.new(url: ENV['REDIS_URL']) }
+  config.redis = ConnectionPool.new(size: 50) { Redis.new(url: ENV['REDIS_URL']) }
   config.average_scheduled_poll_interval = 10
   config.on(:startup) do
     Sidekiq.schedule = YAML.load_file(File.expand_path("../../schedule.yml",__FILE__))
     Sidekiq::Scheduler.reload_schedule!
-    Sidekiq.default_worker_options = { backtrace: 10, retry: false }
   end
 end
 
