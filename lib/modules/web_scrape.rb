d1ad85fcf9f36b766f3749335b65fbfe895af1ce
Daniel Leong <daniel.ltw@gmail.com>
2016-09-18 17:34:42 +1200

    - remove redundant file

--- lib/modules/web_scrape.rb
+++
@@ -1,9 +0,0 @@
-require 'nokogiri'
-require "#{Rails.root}/lib/modules/r_proxy"
-
-module WebScrape
-  include RProxy
-  def nokogiri_open_url(url)
-    return Nokogiri::HTML(RProxy.open_url_with_proxy(url))
-  end
-end
