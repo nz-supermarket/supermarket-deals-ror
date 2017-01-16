bbbe959259a18309539ffdbf43b217cfb289e02e
Daniel Leong <daniel.leong@nzx.com>
2017-01-17 08:06:50 +1300

    Update tor.rb

--- config/initializers/tor.rb
+++ config/initializers/tor.rb
@@ -1,5 +1,5 @@
 Tor.configure do |config|
    config.ip = ENV['TOR_IP']
    config.port = 9050
-   config.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1')
+   config.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2958.0 Safari/537.36')
 end
